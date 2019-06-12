0438197438e97637398f644cc4829862e5c2eaae
Nikola Posa <posa.nikola@gmail.com>
2019-06-12 10:49:57 +0200

    Improve gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,51 +4,55 @@ let rename = require("gulp-rename");
 let uglify = require('gulp-uglify');
 
 // Copy 3rd-party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+gulp.task('vendor', function(done) {
+    gulp.src([
+        './node_modules/bootstrap/dist/**/*',
+        '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+        '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
     .pipe(gulp.dest('./vendor/bootstrap'));
 
-  gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
+    gulp.src([
+        './node_modules/font-awesome/**/*',
+        '!./node_modules/font-awesome/{less,less/*}',
+        '!./node_modules/font-awesome/{scss,scss/*}',
+        '!./node_modules/font-awesome/.*',
+        '!./node_modules/font-awesome/*.{txt,json,md}'
     ])
     .pipe(gulp.dest('./vendor/font-awesome'));
 
-  gulp.src([
+    gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
     .pipe(gulp.dest('./vendor/jquery'));
 
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
+    gulp.src([
+        './node_modules/jquery.easing/*.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+    .pipe(gulp.dest('./vendor/jquery-easing'));
+
+    done();
 });
 
-gulp.task('css:minify', function() {
-  return gulp.src([
-      './css/*.css',
-      '!./css/*.min.css'
+gulp.task('css:minify', function(done) {
+    gulp.src([
+        './css/*.css',
+        '!./css/*.min.css'
     ])
     .pipe(cleanCSS())
     .pipe(rename({
-      suffix: '.min'
+        suffix: '.min'
     }))
     .pipe(gulp.dest('./css'));
+
+    done();
 });
 
-gulp.task('css', gulp.parallel('css:minify'));
+gulp.task('css', gulp.series('css:minify'));
 
-gulp.task('js:minify', function() {
-  return gulp.src([
+gulp.task('js:minify', function(done) {
+    gulp.src([
       './js/*.js',
       '!./js/*.min.js'
     ])
@@ -57,8 +61,10 @@ gulp.task('js:minify', function() {
       suffix: '.min'
     }))
     .pipe(gulp.dest('./js'));
+
+    done();
 });
 
-gulp.task('js', gulp.parallel('js:minify'));
+gulp.task('js', gulp.series('js:minify'));
 
 gulp.task('default', gulp.parallel('css', 'js', 'vendor'));
